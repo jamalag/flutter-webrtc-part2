bc6816d8aaaf14860bce936463b13f3e211c44d6
Amir Eshaq <jamalag@hotmail.com>
2020-06-20 17:09:25 -0400

    .

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatelessWidget { 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
