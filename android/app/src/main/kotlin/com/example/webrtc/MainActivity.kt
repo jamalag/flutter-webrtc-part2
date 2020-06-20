5cea841c5bcdb36344a7849e1dfd63c51f5584f7
Amir Eshaq <jamalag@hotmail.com>
2020-06-20 17:05:45 -0400

    new

---
+++ android/app/src/main/kotlin/com/example/webrtc/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.webrtc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_webrtc/webrtc.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'WebRTC lets learn together'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final _localRenderer = new RTCVideoRenderer();
+
+  @override
+  dispose() {
+    _localRenderer.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    initRenderers();
+    _getUserMedia();
+    super.initState();
+  }
+
+  initRenderers() async {
+    await _localRenderer.initialize();
+  }
+
+  _getUserMedia() async {
+    final Map<String, dynamic> mediaConstraints = {
+      'audio': false,
+      'video': {
+        'facingMode': 'user',
+      },
+    };
+
+    MediaStream stream = await navigator.getUserMedia(mediaConstraints);
+
+    _localRenderer.srcObject = stream;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Container(
+        child: new Stack(
+          children: <Widget>[
+            new Positioned(
+              top: 0.0,
+              right: 0.0,
+              left: 0.0,
+              bottom: 0.0,
+              child: new Container(
+                child: new RTCVideoView(_localRenderer)
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
